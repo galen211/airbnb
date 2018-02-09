82a590095c73169adf085a967a27d76c8de84d75
Galen Simmons <galen.simmons@gmail.com>
2018-02-09 16:25:01 -0500

    updated with competition info

--- lib/sample_script.R
+++ lib/sample_script.R
@@ -1,88 +1,60 @@
-# This R script is based on Sandro's python script, which produces a LB score of 0.8655
-# This script should produce a LB score of 0.86547
+# Load libraries
 
-# load libraries
-library(xgboost)
-library(readr)
-library(stringr)
-library(caret)
-library(car)
+library(randomForest)
+library(magrittr)
+library(dplyr)
 
-set.seed(1)
+# Load data
+path = ""
+train <- read.csv(paste0(path,"train.csv"))
+test <- read.csv(paste0(path,"test.csv"))
+ids = test$id
+test$log_price <- NA
 
-# load data
-df_train = read_csv("../input/train_users.csv")
-df_test = read_csv("../input/test_users.csv")
-labels = df_train['country_destination']
-df_train = df_train[-grep('country_destination', colnames(df_train))]
+all_data <- rbind(train,test)
+train_set = 1:nrow(train)
+test_set <- (nrow(train)+1):(nrow(train) + nrow(test))
 
-# combine train and test data
-df_all = rbind(df_train,df_test)
-# remove date_first_booking
-df_all = df_all[-c(which(colnames(df_all) %in% c('date_first_booking')))]
-# replace missing values
-df_all[is.na(df_all)] <- -1
+# Select a subset of the data
 
-# split date_account_created in year, month and day
-dac = as.data.frame(str_split_fixed(df_all$date_account_created, '-', 3))
-df_all['dac_year'] = dac[,1]
-df_all['dac_month'] = dac[,2]
-df_all['dac_day'] = dac[,3]
-df_all = df_all[,-c(which(colnames(df_all) %in% c('date_account_created')))]
+keep_cols <- c('property_type','room_type','bed_type','cancellation_policy','city',
+               'accommodates','bathrooms','latitude','longitude',
+               'number_of_reviews','review_scores_rating','log_price')
 
-# split timestamp_first_active in year, month and day
-df_all[,'tfa_year'] = substring(as.character(df_all[,'timestamp_first_active']), 1, 4)
-df_all['tfa_month'] = substring(as.character(df_all['timestamp_first_active']), 5, 6)
-df_all['tfa_day'] = substring(as.character(df_all['timestamp_first_active']), 7, 8)
-df_all = df_all[,-c(which(colnames(df_all) %in% c('timestamp_first_active')))]
+all_data <- all_data[,keep_cols]
 
-# clean Age by removing values
-df_all[df_all$age < 14 | df_all$age > 100,'age'] <- -1
+# Impute missing values with 0
 
-# one-hot-encoding features
-ohe_feats = c('gender', 'signup_method', 'signup_flow', 'language', 'affiliate_channel', 'affiliate_provider', 'first_affiliate_tracked', 'signup_app', 'first_device_type', 'first_browser')
-dummies <- dummyVars(~ gender + signup_method + signup_flow + language + affiliate_channel + affiliate_provider + first_affiliate_tracked + signup_app + first_device_type + first_browser, data = df_all)
-df_all_ohe <- as.data.frame(predict(dummies, newdata = df_all))
-df_all_combined <- cbind(df_all[,-c(which(colnames(df_all) %in% ohe_feats))],df_all_ohe)
-
-# split train and test
-X = df_all_combined[df_all_combined$id %in% df_train$id,]
-y <- recode(labels$country_destination,"'NDF'=0; 'US'=1; 'other'=2; 'FR'=3; 'CA'=4; 'GB'=5; 'ES'=6; 'IT'=7; 'PT'=8; 'NL'=9; 'DE'=10; 'AU'=11")
-X_test = df_all_combined[df_all_combined$id %in% df_test$id,]
-
-# train xgboost
-xgb <- xgboost(data = data.matrix(X[,-1]), 
-               label = y, 
-               eta = 0.1,
-               max_depth = 9, 
-               nround=25, 
-               subsample = 0.5,
-               colsample_bytree = 0.5,
-               seed = 1,
-               eval_metric = "merror",
-               objective = "multi:softprob",
-               num_class = 12,
-               nthread = 3
-)
-
-# predict values in test set
-y_pred <- predict(xgb, data.matrix(X_test[,-1]))
-
-# extract the 5 classes with highest probabilities
-predictions <- as.data.frame(matrix(y_pred, nrow=12))
-rownames(predictions) <- c('NDF','US','other','FR','CA','GB','ES','IT','PT','NL','DE','AU')
-predictions_top5 <- as.vector(apply(predictions, 2, function(x) names(sort(x)[12:8])))
-
-# create submission 
-ids <- NULL
-for (i in 1:NROW(X_test)) {
-  idx <- X_test$id[i]
-  ids <- append(ids, rep(idx,5))
+fillna <- function(column) {
+  column[is.na(column)] <- 0
+  return(column)
 }
-submission <- NULL
-submission$id <- ids
-submission$country <- predictions_top5
 
-# generate submission file
-submission <- as.data.frame(submission)
-write.csv(submission, "submission.csv", quote=FALSE, row.names = FALSE)
+col_type <- sapply(all_data,class)
+numeric_type <- !(col_type %in% c("character","factor"))
+all_data[,numeric_type] <- sapply(all_data[,numeric_type], fillna)
+
+# Train a Random Forest model with cross-validation
+
+cv_folds <- sample(1:3, size = nrow(train), replace = TRUE)
+
+for(i in 1:3) {
+  # Train the model using the training sets
+  fit <- randomForest(log_price ~ .,
+                      data = all_data[train_set[cv_folds !=i],],
+                      ntree = 10)
+  
+  # Make predictions using the testing set
+  preds <- predict(fit, all_data[train_set[cv_folds == i],])
+  
+  # Calculate RMSE for current cross-validation split
+  print(mean((preds - all_data[train_set[cv_folds == i],'log_price'])^2)^.5)
+}
+
+# Create submission file
+
+fit <- randomForest(log_price ~ ., data = all_data[train_set,], ntree = 1)
+prediction <- predict(fit, all_data[test_set,])
+
+sample_submission <- data.frame(id = ids, log_price = prediction)
+write.csv(sample_submission, "sample_submission.csv", row.names = FALSE)
